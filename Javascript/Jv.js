3064a517df6c9863ca73a476bf227a755cbb1ba2
Gabriele <gabriele.palma.bool@gmail.com>
2023-12-15 12:00:05 +0100

    coll javascript object with html messages

---
+++ Javascript/Jv.js
@@ -0,0 +1,97 @@
+
+
+
+//Svolgimento
+
+const { createApp } = Vue;
+
+createApp({
+
+    data() {
+
+        return {
+
+            activeAvatar: 0,
+
+            activeMessage: 0,
+
+            contacts: [
+                {
+                    name: 'Vercy',
+                    avatar: './img-recap/avatar-esempi/avatar2.jpeg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message:'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Jack',
+                    avatar: './img-recap/avatar-esempi/avatar5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                }
+            ]
+            
+        }
+    },
+    methods: {
+        
+        
+    },
+
+    mounted() {
+
+    //   for(let i = 0; i < 10;i++){
+ 
+    //      //chiamata tramite libreria axios all'Api ()
+    //      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+    //         .then((risposta) => {
+
+    //            console.log(risposta.data.response);
+
+    //            this.Arraymails.push(risposta.data.response);
+
+    //            console.log(this.Arraymails);
+
+    //            if(i== 9){
+    //                 this.complete = true;
+    //            }
+
+    //         });
+
+    //   }
+;
+  }
+
+}).mount('#app');
